e0279712e728fd89ab3a41d1f7c6e669c63e59b6
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-03-13 10:18:00 +0000

    created data and spec folder

---
+++ src/data.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { name: "hotelone", starRating: 5, facilities: ["car park", "pool"] },
+  { name: "hoteltwo", starRating: 3, facilities: ["car park", "gym"] },
+  { name: "hotelthree", starRating: 3, facilities: [] },
+  {
+    name: "hotelfour",
+    starRating: 4,
+    facilities: ["car park", "pool", "gym"]
+  }
+];

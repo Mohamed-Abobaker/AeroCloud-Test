eca553c2bb95950838b988793240fc5f9672920c
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-03-13 10:28:35 +0000

    add filter checkboxes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import * as data from "./data";
 
 class App extends Component {
   state = {
-    hotels: data
+    hotels: data,
+    arr1: ["car park", "pool", "gym"]
   };
 
   render() {
-    const { hotels } = this.state;
+    const { hotels, arr1 } = this.state;
     return (
       <div className="App">
         <h1>Hotels</h1>
+        <form>
+          <p>Filter By: </p>
+          {arr1.map(facility => {
+            return (
+              <div key={facility}>
+                <label htmlFor={`${facility}`}>{facility}</label>
+                <input
+                  type="checkbox"
+                  id={facility}
+                  value={facility}
+                  onClick={this.handleCheckBox}
+                />
+              </div>
+            );
+          })}
+          <br />
+        </form>
+        <h2>Results</h2>
         {hotels.map(hotel => {
           return (
             <p key={hotel.name}>

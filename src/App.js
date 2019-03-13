df46d694b2fcdbe4dd35efc8d0304d8e32122381
Mohamed-Abobaker <mohamedabobaker17@gmail.com>
2019-03-13 10:22:39 +0000

    display hotels

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import * as data from "./data";
 
 class App extends Component {
+  state = {
+    hotels: data
+  };
+
   render() {
+    const { hotels } = this.state;
     return (
       <div className="App">
         <h1>Hotels</h1>
+        {hotels.map(hotel => {
+          return (
+            <p key={hotel.name}>
+              Name: {hotel.name}, Rating: {hotel.starRating}
+            </p>
+          );
+        })}
       </div>
     );
   }
